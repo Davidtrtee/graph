3e1cb0ea6581204096dd6eae57472cb1b698fb5c
Md <shoyebmd424@gmail.com>
2024-03-21 16:39:01 +0530

    axios base url

--- client/src/ApiService/Axios/index.jsx
+++ client/src/ApiService/Axios/index.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
-const Axios = axios.create({ baseURL: "http://localhost:5500/api" });
+// const Axios = axios.create({ baseURL: "http://localhost:5500/api" });
+const Axios = axios.create({ baseURL: "http://185.124.109.241/api" });
 
 export default Axios;
