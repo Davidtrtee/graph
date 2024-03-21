017087090b1efb2d2339528f99d74c0472de36ea
Md <shoyebmd424@gmail.com>
2024-03-21 07:00:29 +0530

    server test with client

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = require("./Config/expressConfig");
 const PORT = process.env.PORT || 5500;
 Connection();
+app.get("/test", (req, res) => {
+  res.status(200).json("server working fine");
+});
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
