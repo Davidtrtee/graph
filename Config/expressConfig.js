a9b81fef3f4535bd7756150e85968e9e0b7c1edf
Md <shoyebmd424@gmail.com>
2024-03-21 14:34:16 +0530

    config

--- Config/expressConfig.js
+++ Config/expressConfig.js
@@ -6,18 +6,28 @@ const Router = require("../Route/AllRouter");
 dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://185.124.109.241",
-      "https://cosmo-portal.digital",
-      "cosmo-portal.digital",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const whitelist = [
+  "http://localhost:3000",
+  "http://185.124.109.241",
+  "https://cosmo-portal.digital",
+  "http://cosmo-portal.digital",
+  "185.124.109.241",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use("/api", Router);
