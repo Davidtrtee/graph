80553bd47e6ab294285b51141baf9249c1edb694
Md <shoyebmd424@gmail.com>
2024-03-21 14:53:05 +0530

    cors

--- Config/expressConfig.js
+++ Config/expressConfig.js
@@ -6,7 +6,28 @@ const Router = require("../Route/AllRouter");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://185.124.109.241",
+  "https://cosmo-portal.digital",
+  "http://cosmo-portal.digital", // Added http version without 's'
+];
+
+// Configure CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
