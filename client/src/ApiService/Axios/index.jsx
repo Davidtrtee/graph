2aab1bdf26faa2bb15890080f0aa88d44113ce9d
Md <shoyebmd424@gmail.com>
2024-03-23 14:54:00 +0530

    axios url change

--- client/src/ApiService/Axios/index.jsx
+++ client/src/ApiService/Axios/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 // const Axios = axios.create({ baseURL: "http://localhost:5500/api" });
-const Axios = axios.create({ baseURL: "http://185.124.109.241/api" });
+const Axios = axios.create({ baseURL: "https://cosmo-portal.digital/api" });
 
 export default Axios;
